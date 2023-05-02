adfafbbe978a9c5f42aa25b9cda74546fa0b9478
Asif Al Fattah <70211268+AsifAlFattah@users.noreply.github.com>
2023-05-02 12:29:31 +0600

    Add files via upload

---
+++ src/quizmaster/QuizMaster.java
@@ -0,0 +1,16 @@
+
+package quizmaster;
+
+/**
+ *
+ * @author Asif & Noman
+ */
+public class QuizMaster {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
